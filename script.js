5bbd7dc423fb6e114ec154c917a779212e947111
Viviane Aguiar <83668265+vivianezzt@users.noreply.github.com>
2025-07-22 10:31:00 -0300

    feat: Adiciona a logica de implementação do projeto para criptografar

--- script.js
+++ script.js
@@ -1,1 +1,73 @@
-const alfabeto = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+const alfabeto = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+const seletor = document.getElementById("deslocamento");
+const texto = document.getElementById("para-criptografar");
+const botao = document.getElementById("botao");
+const resposta = document.getElementById("resposta");
+
+// Preenche as opções do seletor com o alfabeto
+for (let i = 0; i < alfabeto.length; i++) {
+  seletor.innerHTML += `<option value="${i}">${alfabeto[i]}</option>`;
+}
+
+botao.addEventListener("click", () => {
+  let textoParaCriptografar = texto.value;
+  let deslocamento = +seletor.value;
+
+  let cifrado = cifrar(textoParaCriptografar, deslocamento);
+  resposta.textContent = cifrado;
+  resposta.classList.remove("invisivel");
+});
+
+function cifrar(texto, deslocamento) {
+  let textoMaiusculo = texto.toUpperCase().split("");
+  let textoCriptografado = [];
+
+  for (let i = 0; i < textoMaiusculo.length; i++) {
+    let indiceDaLetra = alfabeto.indexOf(textoMaiusculo[i]);
+    if (indiceDaLetra >= 0) {
+      textoCriptografado.push(letraPorIndice(indiceDaLetra + deslocamento));
+    } else {
+      textoCriptografado.push(textoMaiusculo[i]);
+    }
+  }
+
+  return textoCriptografado.join("");
+}
+
+function letraPorIndice(indice) {
+  let indiceFinal;
+  if (indice >= 0) {
+    indiceFinal = indice % alfabeto.length;
+  } else {
+    indiceFinal = alfabeto.length + (indice % alfabeto.length);
+  }
+  return alfabeto[indiceFinal];
+}
